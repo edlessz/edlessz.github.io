b027beff5c530bad8119c675f34de800d4d5a083
edlessz <42257904+edlessz@users.noreply.github.com>
2025-04-25 16:44:45 -0400

    Removed border

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const setWave = (radius) => {
-  const unit_size = "em";
-  const size = 1;
-  const height = radius + 3 / 3;
-  const p = height * size;
-  const r = +Math.sqrt(p * p + size * size).toFixed(2);
-  const mask = `radial-gradient(${r}${unit_size} at 50% calc(100% - ${+(
-    1 * size +
-    p
-  ).toFixed(2)}${unit_size}),#000 99%,#0000 101%) calc(50% - ${
-    2 * size
-  }${unit_size}) 0/${4 * size}${unit_size} 100%,
-radial-gradient(${r}${unit_size} at 50% calc(100% + ${+p.toFixed(
-    2
-  )}${unit_size}),#0000 99%,#000 101%) 50% calc(100% - ${size}${unit_size})/${
-    4 * size
-  }${unit_size} 100% repeat-x`;
-
-  document.querySelector(".top").style.setProperty("mask", mask);
-};
-
 const setFaviconColor = (color) => {
   const canvas = document.createElement("canvas");
   canvas.width = 64;
@@ -41,11 +20,6 @@ const setFaviconColor = (color) => {
   }
   link.href = dataUrl;
 };
-
-window.addEventListener("scroll", () => {
-  setWave(window.scrollY / 50);
-});
-setWave(window.scrollY);
 
 const color = `hsl(${Math.random() * 360}deg, 72%, 60%)`;
 document.querySelector(":root").style.setProperty("--accent", color);
